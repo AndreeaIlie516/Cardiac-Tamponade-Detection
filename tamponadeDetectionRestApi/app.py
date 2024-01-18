9517346d954ac18864042b7028384c4831855d12
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2024-01-18 12:09:13 +0200

    Backend modified

--- tamponadeDetectionRestApi/app.py
+++ tamponadeDetectionRestApi/app.py
@@ -3,8 +3,6 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 import cv2
 import numpy as np
-from io import BytesIO
-from PIL import Image
 import prediction
 import os
 
@@ -34,26 +32,19 @@ class GetPredictionOutput(Resource):
 
     def post(self):
         try:
-            # Check if an image file was uploaded
             if 'image' not in request.files:
                 return {"error": "No image file provided"}, 400
 
             image_file = request.files['image']
             if image_file:
-                # # Convert the image to an OpenCV format
-                # image = Image.open(BytesIO(image_file.read()))
-                # image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
                 file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
 
-                # Decode the image from bytes
                 image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-
-                print("Image:", image)
 
                 if image is None:
                     return {"error": "Invalid image format or corrupted file"}, 400
-                # Process and predict using your existing logic
+
                 tamponadeDetection = prediction.TamponadeDetection()
                 predictOutput = tamponadeDetection.predict(image)  # New method to handle in-memory images
                 return jsonify(predictOutput)
